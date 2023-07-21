6dd13a4bf04da028b4230cc3186df1515def715c
Oussama-Ouissa1 <140167166+Oussama-Ouissa1@users.noreply.github.com>
2023-07-21 13:41:19 +0200

    Add files via upload

---
+++ prj3.js
@@ -0,0 +1,25 @@
+function open1(){
+  var op = document.getElementById('mdl')
+  op.style.display='block' 
+  var x = document.getElementById ('bd')
+  x.style.opacity="0.1";
+  x.style.backgroundColor="black"
+}
+
+function close1(){
+  var op = document.getElementById('mdl')
+  op.style.display='none' 
+}
+
+function open2(){
+  var op = document.getElementById('mdl1')
+  op.style.display='block' 
+  var x = document.getElementById ('bd')
+  x.style.opacity="0.1";
+  x.style.backgroundColor="black"
+}
+
+function close3(){
+  var op = document.getElementById('mdl1')
+  op.style.display='none' 
+}
